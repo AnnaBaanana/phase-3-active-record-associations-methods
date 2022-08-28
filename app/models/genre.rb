9cfedaf3845afe1cd87328e2ade03874badf4c08
Anna Shulyak <ourmac@Ours-iMac.fios-router.home>
2022-08-28 15:52:05 -0400

    commit tree 1364795cd78081af718517541ea9c7d9512367d0

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,13 +4,27 @@ class Genre < ActiveRecord::Base
 
   def song_count
     # return the number of songs in a genre
+    self.songs.count
   end
 
   def artist_count
-    # return the number of artists associated with the genre
+   artists = []
+   self.songs.each do |song|
+    artists << song.artist_id
+   end
+   artists.uniq.count
   end
 
   def all_artist_names
     # return an array of strings containing every musician's name
+    artists = []
+   self.songs.each do |song|
+    artists << song.artist.id
+   end
+   names = []
+   artists.each do |artist_id|
+    names << Artist.find(artist_id).name
+   end
+   names.uniq
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,12 +3,20 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
+    id = self.genre_id
+    Genre.find_by(:id => id).name
   end
 
   def drake_made_this
     # when this method is called it should assign the song's artist to Drake
     # Drake doesn't exist in the database as an artist yet, so you'll have to create a record
     # Hint: you won't want to create an artist record every time this method is called, only if Drake is *not found*
-
+    if Artist.find_by(:name => 'Drake') 
+      self.artist_id = Artist.find_by(:name => 'Drake').id
+    else
+    Artist.create(name: 'Drake')
+    self.artist_id = Artist.find_by(:name => 'Drake').id
+    end
   end
+  
 end
